f1d369e8213c0ca72e48e371fc5083e2dc9b56b7
thomaswade2000 <cosimawade@gmail.com>
2026-03-11 05:44:03 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy
+import pydirectinput
+import mss
+import pygetwindow
+import logging
+
+
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
